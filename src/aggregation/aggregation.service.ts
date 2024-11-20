766d011f1ec2f89d5e0a3a9b68dc99942c715a31
Jesse Batstone <jb@enablment.com>
2024-11-20 20:21:54 +0100

    Make money look nice by truncating float decimals

--- src/aggregation/aggregation.service.ts
+++ src/aggregation/aggregation.service.ts
@@ -9,7 +9,7 @@ export class AggregationService {
   constructor(
     @InjectRepository(TransactionEntity)
     private readonly transactionRepository: Repository<TransactionEntity>,
-    private readonly configService: ConfigService, 
+    private readonly configService: ConfigService, // Inject ConfigService
   ) {}
 
   async getAggregatedDataByUserId(userId: string) {
@@ -21,17 +21,17 @@ export class AggregationService {
       .select('SUM(CASE WHEN transaction.type = \'earned\' THEN transaction.amount ELSE 0 END)', 'earned')
       .addSelect('SUM(CASE WHEN transaction.type = \'spent\' THEN transaction.amount ELSE 0 END)', 'spent')
       .addSelect(
-        `ROUND(SUM(CASE WHEN transaction.type = 'payout' THEN transaction.amount ELSE 0 END) * ${exchangeRate}, 2)`,
+        `TRUNCATE(SUM(CASE WHEN transaction.type = 'payout' THEN transaction.amount ELSE 0 END) * ${exchangeRate}, 2)`,
         'payout',
       )
       .addSelect(
-        `ROUND(SUM(CASE WHEN transaction.type = 'paid_out' THEN transaction.amount * ${exchangeRate} ELSE 0 END), 2)`,
+        `TRUNCATE(SUM(CASE WHEN transaction.type = 'paid_out' THEN transaction.amount * ${exchangeRate} ELSE 0 END), 2)`,
         'paid_out',
       )
       .addSelect(
         `SUM(CASE WHEN transaction.type = 'earned' THEN transaction.amount ELSE 0 END) - ` +
         `SUM(CASE WHEN transaction.type = 'spent' THEN transaction.amount ELSE 0 END) - ` +
-        `ROUND(SUM(CASE WHEN transaction.type = 'payout' THEN transaction.amount ELSE 0 END) * ${exchangeRate}, 2)`,
+        `TRUNCATE(SUM(CASE WHEN transaction.type = 'payout' THEN transaction.amount ELSE 0 END) * ${exchangeRate}, 2)`,
         'balance',
       )
       .where('transaction.userId = :userId', { userId })
@@ -44,7 +44,7 @@ export class AggregationService {
     const result = await this.transactionRepository
       .createQueryBuilder('transaction')
       .select('transaction.userId', 'userId')
-      .addSelect('ROUND(SUM(transaction.amount), 2)', 'payoutAmount') // Round to 2 decimal places
+      .addSelect('TRUNCATE(SUM(transaction.amount), 2)', 'payoutAmount') // Truncate to 2 decimal places
       .where('transaction.type = :type', { type: 'payout' })
       .groupBy('transaction.userId')
       .getRawMany();
